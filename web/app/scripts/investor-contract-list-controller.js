91ad751dc7d940e0ed781f975dd54e8991e0ccda
Matias Garat Ortiz <matiasgaratortiz@gmail.com>
2016-06-13 15:25:03 -0300

    Add: Sell contract.

--- web/app/scripts/investor-contract-list-controller.js
+++ web/app/scripts/investor-contract-list-controller.js
@@ -3,19 +3,63 @@
  * @classdesc
  * @ngInject
  */
-function InvestorContractListController($scope, $log, $interval, PeerService) {
+function InvestorContractListController($scope, $log, $interval, $uibModal, PeerService) {
 
   var ctl = this;
-  
+
   var init = function() {
-    ctl.list = PeerService.getInvestorContracts();    
+    ctl.list = PeerService.getInvestorContracts();
+    ctl.list.forEach(function (list) {
+      var trade = PeerService.getTrade(list.id);
+      list.price = trade[0].price;
+    });
   };
-  
+
   $scope.$on('$viewContentLoaded', init);
-  
+
   $interval(init, 1000);
 
+  ctl.open = function(trade) {
+    var modalInstance = $uibModal.open({
+      templateUrl: 'sell-contract-modal.html',
+      controller: 'SellModalController as ctl',
+      resolve: {
+        trade: function() {
+          return trade;
+        }
+      }
+    });
+
+    modalInstance.result.then(function(o) {
+      PeerService.sellContract(o);
+    });
+  };
+
+  ctl.create = function() {
+
+  };
+
+  ctl.cancel = function() {
+    modalInstance.dismiss('cancel');
+  };
 }
 
+function SellModalController($uibModalInstance, cfg, trade) {
+
+  var ctl = this;
+
+  ctl.trade = trade;
+
+  ctl.ok = function () {
+    $uibModalInstance.close(ctl.trade);
+  };
+
+  ctl.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
+}
+
+
 angular.module('investorContractListController', [])
-.controller('InvestorContractListController', InvestorContractListController);
+.controller('InvestorContractListController', InvestorContractListController)
+.controller('SellModalController', SellModalController);
--- web/app/scripts/peer-service-mock.js
+++ web/app/scripts/peer-service-mock.js
@@ -8,43 +8,49 @@ function PeerService($log, $q, $http, cfg, UserService) {
   // jshint shadow: true
   var PeerService = this;
   var tradeId = 1100;
-  
+
   PeerService.getIssuerContracts = function() {
-    return _.filter(cfg.contracts, function(o) { 
+    return _.filter(cfg.contracts, function(o) {
       return o.issuerId === UserService.getUser().id;
     });
   };
-  
+
   PeerService.getInvestorContracts = function() {
-    return _.filter(cfg.contracts, function(o) { 
+    return _.filter(cfg.contracts, function(o) {
       return o.ownerId === UserService.getUser().id;
     });
   };
-  
+
   PeerService.getOffers = function() {
-    return _.filter(cfg.trades, function(o) { 
+    return _.filter(cfg.trades, function(o) {
       return o.state === 'offer';
     });
   };
-  
+
+  PeerService.getTrade = function(id) {
+    return _.filter(cfg.trades, function(o) {
+      return o.contractId === id;
+    });
+  };
+
   PeerService.getBonds = function() {
-    return _.filter(cfg.bonds, function(o) { 
+    return _.filter(cfg.bonds, function(o) {
       return o.issuerId === UserService.getUser().id;
     });
   };
-  
+
   PeerService.createBond = function(bond) {
     bond.issuerId = UserService.getUser().id;
     //TODO calculate maturity date from term, use it for the id in place of term
     bond.id = bond.issuerId + '.' + bond.term + '.' + bond.rate;
-    
+
     //TODO fail if bond with this id already exists
     cfg.bonds.push(bond);
-    
+
     // create contracts and offer them for sale: create trades
     var numContracts = bond.principal / 100000;
     var i;
-    
+
     for(i=0; i < numContracts; i++) {
       var contract = {
           id: bond.id + '.' + i,
@@ -53,7 +59,7 @@ function PeerService($log, $q, $http, cfg, UserService) {
           ownerId: bond.issuerId,
           couponsPaid: 0
       };
-      
+
       var trade = {
           id: tradeId++,
           contractId: contract.id,
@@ -66,24 +72,43 @@ function PeerService($log, $q, $http, cfg, UserService) {
       cfg.trades.push(trade);
     }
   };
-  
+
   PeerService.buyContract = function(trade) {
     var buyerId = UserService.getUser().id;
-    
+
     var t = _.find(cfg.trades, function(o) {
       return o.id === trade.id;
     });
-    
-    //TODO first put the trade in captured state 
+
+    //TODO first put the trade in captured state
     // later payment oracle sets it to settled
     t.state = 'settled';
     t.buyerId = buyerId;
-    
+
     var c = _.find(cfg.contracts, function(o) {
       return o.id === trade.contractId;
     });
-    
+
     c.ownerId = buyerId;
+  };
+
+  PeerService.sellContract = function(trade) {
+    var sellerId = UserService.getUser().id;
+
+    var t = _.find(cfg.trades, function(o) {
+      return o.contractId === trade.id;
+    });
+
+    // TODO first put the trade in captured state
+    // later payment oracle sets it to settled
+    t.state = 'offer';
+    t.sellerId = sellerId;
+
+    var c = _.find(cfg.contracts, function(o) {
+      return o.id === trade.id;
+    });
+
+    c.ownerId = sellerId;
   };
 
 }
